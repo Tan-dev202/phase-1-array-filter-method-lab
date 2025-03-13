a4885c579f8c768af43d3767e86e40b11fc8eab5
Enock Tangus <tanguskipngeno@gmail.com>
2025-03-13 11:58:28 +0300

    array filter method lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(driverName => {
+       return driverName.toLowerCase() === name.toLowerCase()
+    });
+}
+
+function fuzzyMatch(drivers, initialName) {
+    let nameLength = initialName.length;
+    return drivers.filter(driverName => {
+        return driverName.slice(0, nameLength) === initialName;
+    });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    });
+}
